fe5cb67ae2c3199eb11449cf68c2fdd134fb793d
KangHannarae <hann52156@gmail.com>
2025-09-02 12:40:22 +0900

    aa

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import { Routes, Route } from 'react-router-dom';
-import { BrowserRouter } from 'react-router-dom';
+// App.js
+import React from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
-import Main from './components/Main';
-import ProblemSolving from './components/ProblemSolving';
-import DailyFortune from './components/DailyFortune'; // 오늘의 운세 컴포넌트 추가
+import Main from "./components/Main";
+import ProblemSolving from "./components/ProblemSolving";
+import DailyFortune from "./components/DailyFortune";
 
 function App() {
-  console.warn = function no_console() { };
+  // 콘솔 경고 제거
+  console.warn = function no_console() {};
 
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
-        <Route path='/' element={<Main />} />
-        <Route path='/problem' element={<ProblemSolving />} />
-        <Route path='/fortune' element={<DailyFortune />} /> {/* 오늘의 운세 라우트 추가 */}
+        <Route path="/" element={<Main />} />
+        <Route path="/problem" element={<ProblemSolving />} />
+        <Route path="/fortune" element={<DailyFortune />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
